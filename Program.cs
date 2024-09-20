555461ae8da41fc5a678ef46b701379303fe1a8c
Seaky <142885534+seakyy@users.noreply.github.com>
2024-09-20 15:57:12 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Music_Player
+namespace missPacMan
 {
     internal static class Program
     {
+
+        
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         static void Main()
         {
+         
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            
         }
     }
 }
